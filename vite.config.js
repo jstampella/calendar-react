09e055c85614a15972e0f95a5308d7cbe2f7bc4f
jonathan <jona.stampella@gmail.com>
2023-02-04 19:21:20 -0300

    fix config vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		commonjsOptions: {
+			transformMixedEsModules: true,
+			exclude: [
+				'node_modules/lodash-es/**',
+				'node_modules/@types/lodash-es/**',
+			],
+		},
+	},
+});
