08ead74742a19ce2d1bc93cf800d4b1df4015d5c
jonathan <jona.stampella@gmail.com>
2023-02-04 20:07:48 -0300

    fix pages

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: './',
 	plugins: [react()],
-	build: {
-		commonjsOptions: {
-			transformMixedEsModules: true,
-			exclude: [
-				'node_modules/lodash-es/**',
-				'node_modules/@types/lodash-es/**',
-			],
-		},
-	},
 });
